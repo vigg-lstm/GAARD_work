29d2e88a2c67e1ed878f8f468f9d84731c61f4e9
EricRLucas <eric.lucas@lstmed.ac.uk>
2022-11-07 22:58:51 +0000

    Changed H12.py script and ran on laptop

--- randomisations/H12/H12.py
+++ randomisations/H12/H12.py
@@ -1,7 +1,6 @@
 from sys import argv
 import numpy as np
 import pandas as pd
-from pathlib import Path
 import malariagen_data
 import allel
 
@@ -12,25 +11,14 @@ if (len(argv) == 4):
 else:
     raise Exception("Fail. There should be three command line arguments (cohort, contig, num_randomisations).")
 
-def h12_gwss(
-        ht,
-        pos,
-        window_size,
-    ):
-
-        h1, h12, h123, h2_h1 = allel.moving_garud_h(ht, size=window_size)
-
-        x = allel.moving_statistic(pos, statistic=np.mean, size=window_size)
-
-        return x, h12
-
+window_size = 1000
 
 ag3 = malariagen_data.Ag3(pre=True)
 metadata = ag3.sample_metadata("3.2")
-meta = metadata.query("sex_call == 'F'")
+metadata = metadata.query("sex_call == 'F'")
 sibs = pd.read_csv("../../NGSrelate/full_relatedness/sib_group_table.csv", sep="\t")
 exclude = sibs.query("keep == False")['sample.name']
-metadata = meta.query("partner_sample_id not in @exclude").reset_index(drop=True)
+metadata = metadata.query("partner_sample_id not in @exclude").reset_index(drop=True)
 
 randomisations = pd.read_csv("../phenotype_randomisations.csv", sep="\t")
 #### make sure all metadata samples are in the randomisations file (so GAARD data)
@@ -53,54 +41,52 @@ gt = allel.GenotypeArray(ds_haps["call_genotype"].data)
 ac = gt.count_alleles()
 biallelic_mask = ac.is_biallelic()
 
-print(f"removing biallelics for {cohort} {contig}, {biallelic_mask.sum()} snps, array is shape: {ds_haps['call_genotype'].shape}")
 ds_haps = ds_haps.isel(variants=biallelic_mask)
-print(f" for {cohort} {contig} after, array is shape: {ds_haps['call_genotype'].shape}")
-pos = ds_haps["variant_position"].values
 ht = gt[biallelic_mask, :].to_haplotypes()
 metadata.set_index('sample_id', inplace = True)
 hap_ids = np.repeat(metadata.loc[ds_haps.sample_id,'partner_sample_id'].values, 2)
 
+pos = ds_haps["variant_position"].values
+midpoints = allel.moving_statistic(pos, statistic=np.mean, size=window_size)
+startpoints = allel.moving_statistic(pos, statistic=np.min, size=window_size)
+endpoints = allel.moving_statistic(pos, statistic=np.max, size=window_size)
+
 alive_dead_dict = {}
 alive_dead_dict['alive'] = random.query(f"phenotype == 'alive' & population == @cohort")['specimen']
 alive_dead_dict['dead']  = random.query(f"phenotype == 'dead' & population == @cohort")['specimen']
-for pheno in ['alive', 'dead']:
-
-    if Path(f"H12_outputs/H12_{cohort}_{pheno}.{contig}.tsv").exists():
-        print(f"skipping {cohort} {pheno} {contig}")
-        continue
+h12_df_dict = {}
+for pheno in ['alive']:
 
     subsample_ht = ht[:, np.isin(hap_ids, alive_dead_dict[pheno])]
     print(f"--------- Running H12 on {cohort} {pheno} | Chromosome {contig} ----------")
-    x, h12 = h12_gwss(
-        subsample_ht,
-        pos,
-        window_size=1000,
-    )
-
-    h12_df = pd.DataFrame({'midpoint':x, 'h12':h12})
-    h12_df.to_csv(f"H12_outputs/H12_{cohort}.{pheno}.{contig}.tsv", sep="\t")
+    h1, h12, h123, h2_h1 = allel.moving_garud_h(subsample_ht, size=window_size)
+    h12_df_dict[pheno] = pd.DataFrame({'startpoint': startpoints, 
+                                       'endpoints': endpoints, 
+                                       'midpoint':midpoints, 
+                                       'h12':np.round(h12, 3)})
 
 # And now the randomisations
+randomised_h12_dict = {'alive': dict(), 'dead': dict()}
 for i in np.arange(1, num_randomisations+1):
     i = '%0.4d' % i
 
     alive_dead_dict['alive'] = random.query(f"r{i} == 'alive' & population == @cohort")['specimen']
     alive_dead_dict['dead']  = random.query(f"r{i} == 'dead' & population == @cohort")['specimen']
 
-    for pheno in ['alive', 'dead']:
+    for pheno in ['alive']:
 
-        if Path(f"H12_outputs/H12_{cohort}_{pheno}{i}.{contig}.tsv").exists():
-            print(f"skipping {cohort} {pheno} {i} {contig}")
-            continue
-
-        print(f"--------- Running H12 on {cohort} {pheno} randomisation {i} | Chromosome {contig} ----------")
         subsample_ht = ht[:, np.isin(hap_ids, alive_dead_dict[pheno])]
-        x, h12 = h12_gwss(
-            subsample_ht,
-            pos,
-            window_size=1000,
-        )
+        print(f"--------- Running H12 on {cohort} {pheno} randomisation {i} | Chromosome {contig} ----------")
+        h1, h12, h123, h2_h1 = allel.moving_garud_h(subsample_ht, size=window_size)
+        randomised_h12_dict[pheno][f"r{i}"] = np.round(h12, 3)
 
-        h12_df = pd.DataFrame({'midpoint':x, 'h12':h12})
-        h12_df.to_csv(f"H12_outputs/H12_{cohort}_{pheno}{str(i)}.{contig}.tsv", sep="\t")
+randomised_h12_df_alive = pd.DataFrame(randomised_h12_dict['alive'])
+#randomised_h12_df_dead = pd.DataFrame(randomised_h12_dict['dead'])
+h12_df_alive = pd.concat([h12_df_dict['alive'], randomised_h12_df_alive], axis = 1)
+#h12_df_dead = pd.concat([h12_df_dict['dead'], randomised_h12_df_dead], axis = 1)
+
+h12_df_alive.to_csv(f"H12_outputs/H12_{cohort}.alive.{contig}.tsv", sep="\t", index = False)
+#h12_df_dead.to_csv(f"H12_outputs/H12_{cohort}.dead.{contig}.tsv", sep="\t", index = False)
+
+
+
