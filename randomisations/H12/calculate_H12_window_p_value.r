7ebba86b2b81792370c900d7e3911f8917a5dd41
EricRLucas <eric.lucas@lstmed.ac.uk>
2022-11-10 14:40:31 +0000

    redrew H12 plots

--- randomisations/H12/calculate_H12_window_p_value.r
+++ randomisations/H12/calculate_H12_window_p_value.r
@@ -95,69 +95,9 @@ load.and.combine.data <- function(pop, find.peaks = T, calculate.P.values = T){
 cat('Loading data\n')
 h12.tables <- lapply(study.pops, function(pop) {cat(pop, '\n'); load.and.combine.data(pop)})
 
-# Write a function to add transparency to a colour
-add.transparency <- function(col, prop.alpha){
-	rgb.val <- col2rgb(col)
-	new.rgb <- rgb(rgb.val[1], rgb.val[2], rgb.val[3], max = 255, alpha = (1 - prop.alpha) * 255)
-	new.rgb
-}
+source('../../shared_functions/R_plotting.r')
 
 chrom.sizes <- c('2R' = 61545105, '2L' = 49364325, '3R' = 53200684, '3L' = 41963435, 'X' = 24393108)
-
-# Write a function to draw the chromosomes on an existing plotting device
-add.chromosomes <- function(gaps, cs, ce, gene.cex = 0.9, gene.col = 'grey20', point.cex = 1.2, point.col = 'grey30', chrom.col = NULL, chrom.cex = 1.4, chrom.offset = 0){
-	plot(c(cs[1], ce[5]), c(-6.5,1.3), xlim = c(cs[1] + gaps/2, ce[5] - gaps/2), type = 'n', bty = 'n', xaxt = 'n', yaxt = 'n', xlab = '', ylab = '')
-	# show the kdr region
-	kdr.region.mean <- cs['2L'] + mean(c(2358158, 2431617))
-	points(kdr.region.mean, 0, pch = 19, cex = point.cex, col = point.col)
-	text(kdr.region.mean, -1.7, 'Vgsc', srt = 45, adj = 1.1, xpd = T, cex = gene.cex, col = gene.col, font = 2)
-	# show the Rdl region
-	rdl.region.mean <- cs['2L'] + mean(c(25363652, 25434556))
-	points(rdl.region.mean, 0, pch = 19, cex = point.cex, col = point.col)
-	text(rdl.region.mean, -1.7, 'Rdl', srt = 45, adj = 1.1, xpd = T, cex = gene.cex, col = gene.col, font = 2)
-	# show the Carboxylesterase region
-	coeae.region.mean <- cs['2L'] + mean(c(28548433, 28550748))
-	points(coeae.region.mean, 0, pch = 19, cex = point.cex, col = point.col)
-	text(coeae.region.mean, -1.7, 'Coeae2f', srt = 45, adj = 1.1, xpd = T, cex = gene.cex, col = gene.col, font = 2)
-	# show the Ace1 gene region
-	ace1.region.mean <- cs['2R'] + mean(c(3484107, 3495790))
-	points(ace1.region.mean, 0, pch = 19, cex = point.cex, col = point.col)
-	text(ace1.region.mean, -1.7, 'Ace1', srt = 45, adj = 1.1, xpd = T, cex = gene.cex, col = gene.col, font = 2)
-	# show the CYP6 region
-	cyp6.region.mean <- cs['2R'] + mean(c(28463000, 28568000))
-	points(cyp6.region.mean, 0, pch = 19, cex = point.cex, col = point.col)
-	text(cyp6.region.mean, -1.7, 'Cyp6aa1-\nCyp6p2 ', srt = 45, adj = 1.1, xpd = T, cex = gene.cex, col = gene.col, font = 2)
-	# show the GST region
-	gst.region.mean <- cs['3R'] + mean(c(28580000, 28605000))
-	points(gst.region.mean, 0, pch = 19, cex = point.cex, col = point.col)
-	text(gst.region.mean, -1.7, 'Gstu4-\nGste3 ', srt = 45, adj = 1.1, xpd = T, cex = gene.cex, col = gene.col, font = 2)
-	# show the CYP6M2-Z1 region
-	cyp6m2.z1.region.mean <- cs['3R'] + mean(c(6900000, 7030000))
-	points(cyp6m2.z1.region.mean, 0, pch = 19, cex = point.cex, col = point.col)
-	text(cyp6m2.z1.region.mean, -1.7, 'Cyp6m2-\nCyp6z1 ', srt = 45, adj = 1.1, xpd = T, cex = gene.cex, col = gene.col, font = 2)
-	# show the CYP9K1 region
-	cyp9k1.region.mean <- cs['X'] + mean(c(15222000, 15257000))
-	points(cyp9k1.region.mean, 0, pch = 19, cex = point.cex, col = point.col)
-	text(cyp9k1.region.mean, -1.7, 'Cyp9k1', srt = 45, adj = 1, xpd = T, cex = gene.cex, col = gene.col, font = 2)
-	# Plot the outline of the chromosomes
-	chrom.col <- if (!is.null(chrom.col)) chrom.col else c('2R' = 'black', '2L' = 'black', '3R' = 'black', '3L' = 'black', 'X' = 'black')
-	chrom.y <- -8.5 - chrom.offset
-	lines(c(ce['2R'], ce['2R'] - gaps/2, cs['2R'], cs['2R'], ce['2R'] - gaps/2, ce['2R']), 
-		  c(-0.2, -1, -1, 1, 1, 0.2), lwd = 2, col = chrom.col['2R'])
-	text((cs['2R'] + ce['2R'])/2, chrom.y, '2R', adj = 0.5, xpd = NA, cex = chrom.cex)
-	lines(c(cs['2L'], cs['2L'] + gaps/2, ce['2L'], ce['2L'], cs['2L'] + gaps/2, cs['2L']), 
-		  c(0.2, 1, 1, -1, -1, -0.2), lwd = 2, col = chrom.col['2L'])
-	text((cs['2L'] + ce['2L'])/2, chrom.y, '2L', adj = 0.5, xpd = NA, cex = chrom.cex)
-	lines(c(ce['3R'], ce['3R'] - gaps/2, cs['3R'], cs['3R'], ce['3R'] - gaps/2, ce['3R']), 
-		  c(-0.2, -1, -1, 1, 1, 0.2), lwd = 2, col = chrom.col['3R'])
-	text((cs['3R'] + ce['3R'])/2, chrom.y, '3R', adj = 0.5, xpd = NA, cex = chrom.cex)
-	lines(c(cs['3L'], cs['3L'] + gaps/2, ce['3L'], ce['3L'], cs['3L'] + gaps/2, cs['3L']), 
-		  c(0.2, 1, 1, -1, -1, -0.2), lwd = 2, col = chrom.col['3L'])
-	text((cs['3L'] + ce['3L'])/2, chrom.y, '3L', adj = 0.5, xpd = NA, cex = chrom.cex)
-	lines(c(cs['X'], cs['X'], ce['X'] - gaps/2, ce['X'], ce['X'], ce['X'] - gaps/2, cs['X']), 
-		  c(-1, 1, 1, 0.2, -0.2, -1, -1), lwd = 2, col = chrom.col['X'])
-	text((cs['X'] + ce['X'])/2, chrom.y, 'X', adj = 0.5, xpd = NA, cex = chrom.cex)
-}
 
 # Function to plot the H12 data
 plot.h12.diff <- function(h12.table, 
@@ -182,8 +122,8 @@ plot.h12.diff <- function(h12.table,
 	ce <- cumsum(chrom.sizes + c(0, 0, gaps, 0, gaps))
 	cs <- ce - chrom.sizes
 	layout(matrix(c(rep(1,4),rep(2,1)), nrow = 5, ncol = 1))
-	colours <- c(h12 = add.transparency('orangered3', 0.2),
-                 randomisations = add.transparency('grey50', 0.8))
+	colours <- c(h12 = add.transparency('limegreen', 0.2),
+                 randomisations = add.transparency('grey50', 0.85))
 	par(mar = c(0,4,1,2), mgp = c(2, 0.7, 0), family = 'Arial', xpd = NA) 
 	num.randomisations <- ifelse(is.null(num.randomisations), 
 	                             length(randomisation.ids),
@@ -220,7 +160,7 @@ plot.h12.diff <- function(h12.table,
 	
 	# Now plot all chromosomes with, the position of each of the four detox gene regions and Ace1
 	par(mar = c(1,4,0,2), mgp = c(2, 0.7, 0)) 
-	add.chromosomes(gaps = gaps, cs = cs, ce = ce, gene.cex = 0.7, point.cex = 1, chrom.offset = -1.2, chrom.cex = 1.2)
+	add.chromosomes(chrom.sizes, gaps = gaps, gene.cex = 0.7, point.cex = 1, chrom.offset = -1.2, chrom.cex = 1.2)
 	
 	if (!missing(filename))
 		dev.off()
